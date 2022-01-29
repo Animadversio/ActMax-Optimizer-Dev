d3c8daac870b034abefc2faf065546c40ac62f86
binxu.wang <binxu.wang@wustl.edu>
2022-01-29 14:08:56 -0500

    add demo for ng traces

--- tutorials/nevergrad_baseline_demo.py
+++ tutorials/nevergrad_baseline_demo.py
@@ -10,15 +10,17 @@ from core.CNN_scorers import TorchScorer
 #%%
 G = upconvGAN("fc6")
 G.eval().cuda()
-scorer = TorchScorer("resnet50") # _linf8
-scorer.select_unit((None,'.layer1.Bottleneck2',1,28,28))
-scorer.select_unit((None,'.layer2.Bottleneck3',1,14,14))
-scorer.select_unit((None,'.layer3.Bottleneck5',1,7,7))
-scorer.select_unit((None,'.layer4.Bottleneck2',1,4,4))
+scorer = TorchScorer("resnet50_linf8") #
+# scorer.select_unit((None,'.layer1.Bottleneck2',1,28,28))
+# scorer.select_unit((None,'.layer2.Bottleneck3',1,14,14))
+# scorer.select_unit((None,'.layer3.Bottleneck5',1,7,7))
+# scorer.select_unit((None,'.layer4.Bottleneck2',1,4,4))
 scorer.select_unit((None,'.Linearfc',1))
 def score_batch(z, ):
     return -scorer.score(G.visualize_batch_np(z.reshape([-1,4096])))
 
 instrum = ng.p.Instrumentation(ng.p.Array(shape=(4096,)).set_bounds(-6, 6),)
-optimizer = ng.optimizers.CMA(parametrization=instrum, budget=2800, num_workers=40,)
+#%%
+optimizer = ng.optimizers.CMA(parametrization=instrum, budget=3000, num_workers=40,)
 optimizer.minimize(score_batch, verbosity=True, batch_mode=True)
+
